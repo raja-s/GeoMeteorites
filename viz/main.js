7d410d8e86d37807d0f21d99a418255744ef7cb3
romanoe <noemi.romano@epfl.ch>
2017-11-19 17:41:26 +0100

    Add globe

--- viz/main.js
+++ viz/main.js
@@ -21,3 +21,85 @@ document.getElementById("startButton").onclick = function() {
     let speedFactor = document.getElementById("speedFactor").value;
 
 };
+
+
+//----------------------------------Globe-----------------------------------------------
+
+var width2 = document.getElementById("mapArea").clientWidth,
+    height2 = document.getElementById("mapArea").clientHeight;
+
+var radius = height2 / 2 - 10,
+    scale = radius,
+    velocity = .01;
+
+var projection = d3.geoOrthographic()
+    .translate([width2 / 2, height2 / 2])
+    .scale(scale)
+    .clipAngle(90);
+
+
+
+var canvas = d3.select("#mapArea").append("canvas")
+    .attr("width", width2)
+    .attr("height", height2)
+    .attr("fill","white");
+
+var context2 = canvas.node().getContext("2d");
+
+var path = d3.geoPath()
+    .projection(projection)
+    .context(context2);
+
+
+var graticule = d3.geoGraticule();
+
+
+//Get the json file with countries + rotation + circle around globe + graticule
+var url = "https://unpkg.com/world-atlas@1/world/110m.json";
+
+    d3.json(url, function(error, world) {
+      if (error) throw error;
+
+      //Get countries
+      var countries = topojson.feature(world, world.objects.countries);
+
+
+      d3.timer(function(elapsed) {
+
+
+
+
+        //Remove rectangles of movements
+        context2.clearRect(0, 0, width2, height2);
+
+        //Rotation
+        projection.rotate([velocity * elapsed, 0]);
+
+        //Add countries
+        context2.beginPath();
+        path(countries);
+        context2.strokeStyle = "white";
+        context2.lineWidth = .3;
+        context2.fill();
+        context2.stroke();
+
+        //Add circle aaround the globe
+        context2.beginPath();
+        context2.arc(width2 / 2, height2 / 2, radius, 0, 2 * Math.PI, true);
+        context2.strokeStyle = "black";
+        context2.lineWidth = 2.5;
+        context2.stroke();
+
+        //Add graticule
+        context2.beginPath();
+        path(graticule());
+        context2.strokeStyle = "grey";
+        context2.lineWidth = .3;
+        context2.stroke();
+
+
+
+      });
+    });
+
+    d3.select(self.frameElement).style("height", height2 + "%");

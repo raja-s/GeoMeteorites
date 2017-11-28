136a9fc4fb36c04e809864b85183f51fb3c8abe2
RajaSoufi <rjsoufi@gmail.com>
2017-11-28 12:18:53 +0100

    Slightly decreased number of operations in main loop of dropMeteorite

--- viz/meteorites.js
+++ viz/meteorites.js
@@ -15,8 +15,6 @@ const EXPLOSION_DURATION = 600;
 
 const STEP_TIME  = 16;
 
-const METEORITES = [];
-
 const METEORITE_GEOMETRY = new THREE.SphereGeometry(1, 7, 7);
 
 const MATERIAL = new THREE.MeshBasicMaterial({
@@ -28,41 +26,34 @@ const EXPLOSION_GEOMETRY = new THREE.SphereGeometry(0, 16, 16);
 
 function dropMeteorite(long, lat, mass) {
     
+    // TODO: Check long and lat
+    const PHI   = THREE.Math.degToRad(long);
+    const THETA = THREE.Math.degToRad(lat);
+    
+    const MAX_RANDOM_RADIUS = 2 * Math.PI / 3;
+    
+    const START_PHI   = PHI    + (Math.random() - 0.5) * MAX_RANDOM_RADIUS;
+    const START_THETA = THETA  + (Math.random() - 0.5) * MAX_RANDOM_RADIUS;
+    const START_R     = RADIUS + 200;
+    
+    const STEP_PHI   = (PHI    - START_PHI)   * STEP_TIME / FALL_DURATION;
+    const STEP_THETA = (THETA  - START_THETA) * STEP_TIME / FALL_DURATION;
+    const STEP_R     = (RADIUS - START_R)     * STEP_TIME / FALL_DURATION;
+    
     let meteorite = new THREE.Mesh(METEORITE_GEOMETRY, MATERIAL.clone());
     addToScene(meteorite);
     
     let explosion = new THREE.Mesh(EXPLOSION_GEOMETRY, MATERIAL.clone());
     
-    // let meteoriteLight = new THREE.PointLight(0xffad5b, 2, 1000);
-    // addToScene(meteoriteLight);
-    
-    // TODO: Check long and lat
-    const PHI   = THREE.Math.degToRad(long);
-    const THETA = THREE.Math.degToRad(lat);
-    const R     = RADIUS;
-    
-    const RANDOM_RADIUS = Math.PI / 3;
-    
-    const START_PHI   = PHI    + (Math.random() - 0.5) * RANDOM_RADIUS;
-    const START_THETA = THETA  + (Math.random() - 0.5) * RANDOM_RADIUS;
-    const START_R     = RADIUS + 200;
-    
-    let { x , y , z } = sphericalToCartesian(START_R, START_PHI, START_THETA + (Math.PI / 2));
-    meteorite.position.set(x, y, z);
-    // meteoriteLight.position.set(x, y, z);
-    
-    METEORITES.push(meteorite);
-    
-    const STEP_PHI   = (PHI   - START_PHI)   * STEP_TIME / FALL_DURATION;
-    const STEP_THETA = (THETA - START_THETA) * STEP_TIME / FALL_DURATION;
-    const STEP_R     = (R     - START_R)     * STEP_TIME / FALL_DURATION;
-    
-    let j = 0;
+    let phi   = START_PHI;
+    let theta = START_THETA + (Math.PI / 2);
+    let r     = START_R;
     
     function deltaFall() {
-        j++;
-        let { x , y , z } = sphericalToCartesian(START_R + j * STEP_R,
-            START_PHI + j * STEP_PHI, START_THETA + j * STEP_THETA + (Math.PI / 2));
+        phi   += STEP_PHI;
+        theta += STEP_THETA;
+        r     += STEP_R;
+        let { x , y , z } = sphericalToCartesian(r, phi, theta);
         meteorite.position.set(x, y, z);
     }
     
@@ -77,13 +68,12 @@ function dropMeteorite(long, lat, mass) {
         
         addToScene(explosion);
         
-        let { x , y , z } = sphericalToCartesian(R, PHI, THETA + (Math.PI / 2));
+        let { x , y , z } = sphericalToCartesian(RADIUS, PHI, THETA + (Math.PI / 2));
+        meteorite.position.set(x, y, z);
         explosion.position.set(x, y, z);
         
-        let k = 0;
-        
         function deltaExplosion() {
-            explosion.scale.setLength(++k * STEP_MASS);
+            explosion.scale.addScalar(STEP_MASS);
             explosion.material.opacity -= STEP_OPACITY;
             meteorite.material.opacity -= STEP_OPACITY;
         }

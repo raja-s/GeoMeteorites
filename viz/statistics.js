a72c1df3011aef949776d583b27409e292b41dd9
romanoe <noemi.romano@epfl.ch>
2017-12-08 20:14:19 +0100

    -Horizontal mass distribution -X and Y axis -Menu button fixed

--- viz/statistics.js
+++ viz/statistics.js
@@ -147,33 +147,39 @@ let width = +svg.attr("width") - margin.left - margin.right,
         height = +svg.attr("height") - margin.top - margin.bottom,
         g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-let  x = d3.scaleLinear()
-        .rangeRound([0, width])
-        .domain([0,d3.max(massFell)]);
+
+
+let  y = d3.scaleLinear()
+        .rangeRound([0,height])
+        .domain([0,d3.max(massFound)]);
 
 let bins1 = d3.histogram()
-        .domain(x.domain())
-        .thresholds(x.ticks(40))
+        .domain(y.domain())
+        .thresholds(y.ticks(40))
         (massFound);
 
 
-let bins2 = d3.histogram()
-        .domain(x.domain())
-        .thresholds(x.ticks(40))
-        (massFell);
+
+let  x = d3.scaleLinear()
+                       .domain([0, d3.max(bins1, function(d) {
+                         return d.length;
+                       })])
+                       .range([0,width]);
+
+
+// let bins2 = d3.histogram()
+//         .domain(x.domain())
+//         .thresholds(x.ticks(50))
+//         (massFell);
 
 
 
-// var line = d3.line()
-//                   .x(function(d) { return x((d.x0 + d.x1)/2); })
-//                   .y(function(d) { return y(d.length); })
+// let line = d3.line()
+//                   .y(function(d) { return y((d.x0 + d.x1)/2); })
+//                   .x(function(d) { return x(d.length); })
 //                   .curve(d3.curveCatmullRom.alpha(0.5));
-
-let  y = d3.scaleLinear()
-               .domain([0, d3.max(bins1, function(d) {
-                 return d.length;
-               })])
-               .range([height, 0]);
+//
+//
 
 
 let bar1 = g.selectAll(".bar1")
@@ -181,72 +187,95 @@ let bar1 = g.selectAll(".bar1")
                        .enter().append("g")
                        .attr("class", "bar1")
                        .attr("transform", function(d) {
-                         return "translate(" + x(d.x0) + "," + y(d.length) + ")";
+                         return "translate(0," + y(d.x0) + ")";
                        });
 
-let bar2 = g.selectAll(".bar2")
-            .data(bins2)
-          .enter().append("g")
-          .attr("class", "bar2")
-        .attr("transform", function(d) {
-          return "translate(" + x(d.x0) + "," + y(d.length) + ")";});
+// let bar2 = g.selectAll(".bar2")
+//             .data(bins2)
+//           .enter().append("g")
+//           .attr("class", "bar2")
+//         .attr("transform", function(d) {
+//           return "translate(" + x(d.x0) + "," + y(d.length) + ")";});
 
 
 
 
 bar1.append("rect")
      .attr("x", 0.5)
-     .attr("width", x(bins1[0].x1) - x(bins1[0].x0) - 1)
+     .attr('height',y(bins1[0].x1) - y(bins1[0].x0) +2)
      .transition(t)
-      .attr("height", function(d) {return height - y(d.length);});
+     .attr('width', function(d) {return x(d.length);});
 
-bar2.append("rect")
-           .attr("x", 0.5)
-           .attr("width", x(bins2[0].x1) - x(bins2[0].x0) - 1)
-           .transition(t)
-            .attr("height", function(d) {return height - y(d.length);});
+// g.append("path")
+//        .transition(t)
+//        .attr("d", line(bins1))
+//        .attr("class", "bins1");
+
+// bar2.append("rect")
+//            .attr("x", 0.5)
+//            .attr("width", x(bins2[0].x1) - x(bins2[0].x0) - 1)
+//            .transition(t)
+//             .attr("height", function(d) {return height - y(d.length);});
 
 
-let legend = svg.append("g")
-                    .attr("class", "legend")
-                    .attr("transform", "translate(" + (width - 245) + "," + 40 + ")")
-                    .selectAll("g")
-                    .data(["massFell", "massFound"])
-                    .enter().append("g");
-
-legend.append("text")
-                    .attr("y", function(d, i) {
-                      return i * 30 + 5;
-                    })
-                    .attr("x",100)
-                    .text(function(d) {
-                      return d;
-                    });
-
-                  legend.append("rect")
-                    .attr("y", function(d, i) {
-                      return i * 30 - 8;
-                    })
-                    .attr("x", 80)
-                    .attr("width", 10)
-                    .attr("height", 10)
-                    .attr("fill", function(d) {
-                      if (d == "massFound") {
-                        return 'brown';
-                      } else {
-                        return 'orange';
-                      }
-                    });
-
+// let legend = svg.append("g")
+//                     .attr("class", "legend")
+//                     .attr("transform", "translate(" + (width - 245) + "," + 40 + ")")
+//                     .selectAll("g")
+//                     .data(["massFell", "massFound"])
+//                     .enter().append("g");
+//
+// legend.append("text")
+//                     .attr("y", function(d, i) {
+//                       return i * 30 + 5;
+//                     })
+//                     .attr("x",100)
+//                     .text(function(d) {
+//                       return d;
+//                     });
+//
+//                   legend.append("rect")
+//                     .attr("y", function(d, i) {
+//                       return i * 30 - 8;
+//                     })
+//                     .attr("x", 80)
+//                     .attr("width", 10)
+//                     .attr("height", 10)
+//                     .attr("fill", function(d) {
+//                       if (d == "massFound") {
+//                         return 'brown';
+//                       } else {
+//                         return 'orange';
+//                       }
+//                     });
+//
 
 
 
   g.append("g")
     .attr("class", "axis axis--x")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
+    .attr("transform", "translate(0,"-height +")")
+    .call(d3.axisLeft(y));
+
+  g.append("g")
+      .attr("class", "axis axis--y")
+      .attr("transform", "translate(0,"+height+ ")")
+      .call(d3.axisBottom(x))
 
 
+
+
+    g.append('text')
+    .attr('transform','rotate(-90)')
+    .attr("y", 0 - margin.left -.5)
+    .attr("x",0 - (height / 2))
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .text("log(Mass) [gr]");
+
+
+
+    // text label for the y
 
 });
 
